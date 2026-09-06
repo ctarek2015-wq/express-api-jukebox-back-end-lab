1fc00179baf1756e156888ec8fdbcf3fda287424
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-06 13:23:17 +0300

    add track model with schema for title and artist

---
+++ models/tracks.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+
+const trackSchema = new Schema({
+  title: { type: String, required: true },
+  artist: { type: String, required: true },
+});
+
+const Track = model("Track", trackSchema);
+
+module.exports = Track;
